9465af5607045dbc1436712b0d7564fe5a36857f
StarryNight00 <cisa.matias@gmail.com>
2019-06-07 11:28:38 +0100

    excs Aula13

---
+++ HowManyOfThisType/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HowManyOfThisType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object[] ordenada = { 1, 2, "ab", "ba", 'c', 'd', 17.4f, 1.5f, 20.0f };
+
+            int numeroStrings = GetTotalComponents<string>(ordenada);
+            int numeroFloats = GetTotalComponents<float>(ordenada);
+
+            Console.WriteLine($"num Strings em 'ordenada' é {numeroStrings}");
+            Console.WriteLine($"num Floats em 'ordenada' é {numeroFloats}");
+            Console.ReadKey();
+        }
+
+        private static int GetTotalComponents<T>(IEnumerable someEnumerable)
+        {
+            int total = 0;
+            foreach (object o in someEnumerable)
+            {
+                if (o is T) total++;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ TheyCameBefore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TheyCameBefore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] conjunto = {1, 2, 5, -6, 45, 3, -8, 3};
+
+            int test = 5;
+
+            foreach (int x in GetBeforeThan<int>(conjunto, test))
+                Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<E> GetBeforeThan<E> (IEnumerable<E> someEnumerable, E x) where E:IComparable<E>
+        {
+            foreach(E y in someEnumerable)
+            {
+                if (y.CompareTo(x) < 0)
+                {
+                    yield return y;
+                }
+            }
+        }
+
+    }
+}
